7d3e54408d310f0d1a81313a9a3e7b98126418e5
Jesper Dalgas Zachariassen <jesperzach@gmail.com>
2017-04-19 09:27:20 +0200

    Vipps now properly returns rejected promises.

--- src/clients/vipps.ts
+++ src/clients/vipps.ts
@@ -54,7 +54,7 @@ export class VippsRequest implements IWalletRequest {
 
             function onPollRequestRejected(error?): Promise<IWalletResult> {
                 if (retries >= maximumRetries)
-                    Promise.reject(new ConnectionError("The maximum number of retries has been exceeded."));
+                    return Promise.reject(new ConnectionError("The maximum number of retries has been exceeded."));
 
                 return new Promise<IWalletResult>(
                     resolve => {
@@ -76,7 +76,7 @@ export class VippsRequest implements IWalletRequest {
                         if (events) events.emit("pollRequestFulfilled", response);
 
                         if (!response || !response.meta)
-                            Promise.reject(new NoResponseError("The response was empty."));
+                            return Promise.reject(new NoResponseError("The response was empty."));
 
                         if (!response.meta.result)
                             return onPollRequestRejected();
@@ -84,7 +84,7 @@ export class VippsRequest implements IWalletRequest {
                         if (response.wait) return poll();
 
                         if (!response.authorizeresult)
-                            Promise.reject(new AuthorizationError(response.meta.message.merchant));
+                            return Promise.reject(new AuthorizationError(response.meta.message.merchant));
 
                         return Promise.resolve<IWalletResult>({
                             data: {
